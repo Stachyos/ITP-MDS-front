d2561c1ba955cbe9eaa02d1af3daea3fc75386d1
梁梓航 <2543313@dundee.ac.uk>
2025-09-21 23:20:17 +0800

    good

--- Front/src/utils/globalCfg.js
+++ Front/src/utils/globalCfg.js
@@ -1,3 +1,3 @@
 //本系统服务端地址
 export const ENVI_DEV_SERVER_ADDRESS = 'http://127.0.0.1:7486'; //localhost
-export const ENVI_PROD_SERVER_ADDRESS = '';  //阿里云
+export const ENVI_PROD_SERVER_ADDRESS = 'http://47.243.29.156:7486';  //阿里云
